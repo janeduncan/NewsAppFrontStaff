0b39f836f745d11f9bb32628a29e76cdb09f6578
Fiona Wilson <fionaw237@gmail.com>
2018-10-19 18:45:16 +0100

    Adds some CSS to homepage

--- src/containers/articles/ArticleContainer.js
+++ src/containers/articles/ArticleContainer.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const ArticleContainer = (props) => {
   return (
-    <div className="article-container">
-      article container
+    <div className="grid-item">
+      <h1 className="grid-item-heading">Articles</h1>
+      <div className="buttons-container">
+        <button className="view-all-button">View all</button>
+        <button className="add-new-button">Add new article</button>
+      </div>
     </div>
   )
 }
--- src/containers/journalists/JournalistContainer.js
+++ src/containers/journalists/JournalistContainer.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const JournalistContainer = (props) => {
   return (
-    <div className="journalist-container">
-      journalist container
+    <div className="grid-item">
+      <h1 className="grid-item-heading">Journalists</h1>
+      <div className="buttons-container">
+        <button className="view-all-button">View all</button>
+        <button className="add-new-button">Add new journalist</button>
+      </div>
     </div>
   )
 }
